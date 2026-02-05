feea37074a92a64c7b2e96862bdad7673ff3d334
BOLLA CHARAN REDDY <bollacharanreddy@gmail.com>
2026-02-05 18:00:06 +0530

    Add prediction functionality for skin disease model

---
+++ predict.py
@@ -0,0 +1,14 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+
+model = tf.keras.models.load_model("skin_disease_model.h5")
+
+img_path = "test_image.jpg"
+img = image.load_img(img_path, target_size=(224, 224))
+img_array = image.img_to_array(img)
+img_array = np.expand_dims(img_array, axis=0)
+img_array = img_array / 255.0
+
+prediction = model.predict(img_array)
+print("Prediction:", prediction)
